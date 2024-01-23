9f8bb3d37df69ebab6f7112889a5199f8947bd63
WontonLov3r <danielhariantowu@gmail.com>
2024-01-23 13:51:08 -0500

    yes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //wait for the document to be ready before making a call
-$(document).ready( function() {
-//make a GET request to the JSON API using JQuery's.getJSON method    
-    $.getJSON("schedule.json", function(data){
+$(document).ready(function () {
+    //make a GET request to the JSON API using JQuery's.getJSON method    
+    $.getJSON("schedule.json", function (data) {
         //store the schedule data in a global variable
         let scheduleData = data.schedule;
         //populate the table with all the schedule data
@@ -9,15 +9,15 @@ $(document).ready( function() {
     })
     function populateTable(scheduleData) {
         $('#schedule-table-body').empty();
-$.each(scheduleData. function(i, schedule) {
+        $.each(scheduleData, function (i, schedule) {
 
 
-        let row = "<tr>";
-        row += "<td>" + schedule.class_name + "<tb>";
-        row += "<td>" + schedule.teacher_name + "<tb>";
-        row += "<td>" + schedule.room_number + "<tb>";
-        row += "<td>" + schedule.days.join(', ') + "<tb>";
-        $('#schedule-table-body').append(row)
+            let row = "<tr>";
+            row += "<td>" + schedule.class_name + "<tb>";
+            row += "<td>" + schedule.teacher_name + "<tb>";
+            row += "<td>" + schedule.room_number + "<tb>";
+            row += "<td>" + schedule.days.join(', ') + "<tb>";
+            $('#schedule-table-body').append(row)
+        })
     }
-})
 })
